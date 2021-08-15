c2c8b8ed62ce206252837596f6c4d973ca626bf4
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-15 15:31:55 -0300

    Outras estruturas python

--- 001-estruturas.py
+++ 001-estruturas.py
@@ -1,6 +1,6 @@
 #  estruturas do tipo sequência
 
-# Sequência mutável e ordenada de elementos
+# ====== Sequência mutável e ordenada de elementos ======
 names = ['Cleberton', 'tiago', 'Lucas', 'Fernanda']
 
 # acessando valores
@@ -25,4 +25,48 @@ print(names.index('tiago'))
 # ordena lista
 names.sort()
 print(names)
+
+# ======  Tuplas (tuple) ======
+
+# São similares a listas, porém não podem ser modificados durante a execução do programa
+
+numbers = (1, 5, 3)
+print(numbers)
+print(numbers[2])
+
+# ====== Conjuntos (set) ======
+
+# Conjunto de elementos únicos e não ordenados. Como conjuntos, implementam operações de união, intersecção e outras.
+
+permicoes = { 'user', 'admin'}
+
+permicoes.add('custommer') # caso ja exista nada é adicionado
+
+print(permicoes)
+
+print(permicoes.union({'seller'})) # faz união dos conjuntos
+
+print(permicoes.intersection({'user', 'admin'})) # # retorna um conjunto resultante da intersecção dos conjuntos
+
+print(permicoes.difference({'admin'})) # retorna a diferença entre os dois conjuntos
+
+# criando conjunto com set
+nome = set({"Cleberton"})
+
+print(nome)
+
+# ====== Conjuntos imutáveis (frozenset) ======
+
+# Variação do set, porém imutável, ou seja, seus elementos não podem ser modificados durante a execução do programa.
+
+permissions = frozenset(["member", "group"])  # assim como o set, qualquer estrutura iterável pode ser utilizada para criar um frozenset
+
+permissions.union({"user"})  # novos conjuntos imutáveis podem ser criados à partir do original, mas o mesmo não pode ser modificado
+
+permissions.intersection({"user", "member"})  # retorna um conjunto resultante da intersecção dos conjuntos
+
+permissions.difference({"user"})  # retorna a diferença entre os dois conjuntos
+
+
+# ====== Dicionários (dict) ======
 
