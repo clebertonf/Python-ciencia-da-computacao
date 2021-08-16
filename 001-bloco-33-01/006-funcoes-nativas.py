4e5ca8c553b2661d23cebc4404a4594cf3e8fb51
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-15 21:52:16 -0300

    Funçoẽs nativas

---
+++ 001-bloco-33-01/006-funcoes-nativas.py
@@ -0,0 +1,10 @@
+# Experimente também para funções como abs ou len que são padrões da linguagem. 
+# E para a função sin que está presente no módulo math que já vem embutido.
+
+import math
+
+help(abs)
+
+help(len)
+
+help(math.sin)
