dbd53384a1ccd8c5c447e313bb5489fef971215d
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-16 09:08:37 -0300

    Helpers iniciando

--- 001-bloco-33-01/007-atalhos-console.py
+++ 001-bloco-33-01/007-atalhos-console.py
@@ -6,3 +6,6 @@ print(dir(variavel))
 # mostra o que o metodo em questão faz
 print(help(variavel.capitalize))
 
+# instalar iphython via pip
+# É um terminal mais interativo com recursos mais faceis
+
