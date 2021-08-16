7b07248e7d1922f0aeb8505c4c4c93a109de02b4
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-15 22:18:59 -0300

    Helpers e funçoes possiveis para o tipo de dado

---
+++ 001-bloco-33-01/007-atalhos-console.py
@@ -0,0 +1,8 @@
+# mosta os metodos, possiveis para o tipo de dado da variavel
+
+variavel = "Cleber"
+print(dir(variavel))
+
+# mostra o que o metodo em questão faz
+print(help(variavel.capitalize))
+
