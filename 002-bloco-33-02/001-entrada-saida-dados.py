be857310beb7c216052f3e860eab7b05c3b1d8f4
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-16 22:40:03 -0300

    Saida de dados

--- 002-bloco-33-02/001-entrada-saida-dados.py
+++ 002-bloco-33-02/001-entrada-saida-dados.py
@@ -29,6 +29,39 @@ if __name__ == " __main__":
 # executando (python3 arquivo.py 2 4 "teste")
 
 
-# Print
+# Saida de dados Print
+
+print("O resultado é", 42)  # saída: O resultado é 42
+print("Os resultado são", 6, 23, 42)  # saída: Os resultados são 6 23 42
+
+print("Maria", "João", "Miguel", "Ana")  # saída: Maria João Miguel Ana
+print("Maria", "João", "Miguel", "Ana", sep=", ")  # saída: Maria, João, Miguel, Ana
+
+# print na mesma lina
+print("Na mesma ", end="")
+print("linha.")
+
+# Existe um parâmetro que nos permite modificar a saída padrão para a saída de erros:
+
+import sys
 
 
+err = "Arquivo não encontrado"
+print(f"Erro aconteceu: {err}", file=sys.stderr)
+
+#  Em Python , podemos fazer interpolação de variáveis e expressões utilizando f-string . 
+# Adicionamos um f antes das aspas e o valor de saída entre chaves. Essa dica é importante,
+#  pois é a maneira mais eficiente de formatar strings.
+
+x = 5
+y = 3
+print(f"{x} / {y} = {x / y:.2f}")  # saída: 5 / 2 = 1.67
+# {x} é substituído por 5
+# {y} é substituído por 3
+# {x / y:.2f} O que vem após os dois pontos são formatadores, como nesse exemplo, duas casas decimais (.2f).
+print(f"{x:.^3}")  # saída: ".5."
+# . é o caractere utilizado para preencher
+# ^ indica que o valor será centralizado
+# 3 são o número de caracteres exibidos
+
+# 
