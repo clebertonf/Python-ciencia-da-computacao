49a6f26acafaf227efd2b1f7e46b32f176e57f5f
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-17 10:46:47 -0300

    finalizando parte sobre escrita e leitura

--- 002-bloco-33-02/002-manipulacao-arquivos.py
+++ 002-bloco-33-02/002-manipulacao-arquivos.py
@@ -37,3 +37,33 @@ content = file_3.read()
 print(content)
 file_3.close()
 
+# Um arquivo é também um iterável, ou seja, pode ser percorrido em um laço de repetição. A cada iteração, uma nova linha é retornada.
+
+# Escrita
+file_4 = open('meu_arquivo_04.txt', mode='w')
+LINES = ["Olá\n", "mundo\n", "belo\n", "do\n", "Python\n"]
+file_4.writelines(LINES)
+file_4.close()
+
+# Leitura
+file_4 = open('meu_arquivo_04.txt', mode='r')
+
+for line in file_4:
+    print(line)
+file_4.close()
+
+
+# Além de arquivos textuais como os que manipulamos até agora, temos também arquivos binários.
+#  Eles são arquivos que contêm uma série de bytes e a sua leitura pode variar de acordo com o arquivo.
+#  Nesse caso, devemos acrescentar um b ao parâmetro mode , indicando que será utilizado o modo binário.
+
+# escrita
+file = open("arquivo.dat", mode="wb")
+file.write(b"C\xc3\xa1ssio 30")  # o prefixo b em uma string indica que seu valor está codificado em bytes
+file.close()
+
+# leitura
+file = open("arquivo.dat", mode="rb")
+content = file.read()
+print(content)  # saída: b'C\xc3\xa1ssio 30'
+file.close()  # não podemos esquecer de fechar o arquivo
