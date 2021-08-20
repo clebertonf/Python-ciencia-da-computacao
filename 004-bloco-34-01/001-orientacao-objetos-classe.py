c230ae41eecad1bab2f9fef2383c9118d8ade16f
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-20 16:35:01 -0300

    Criação de uma classe com metodos para exemplo

--- 004-bloco-34-01/001-orientacao-objetos-classe.py
+++ 004-bloco-34-01/001-orientacao-objetos-classe.py
@@ -1,8 +1,9 @@
 # Classe / entidade
 
+
 class User:
     def __init__(self, name, email, password):
-        """ Método construtor da classe User. Note que
+        """Método construtor da classe User. Note que
         o primeiro parâmetro deve ser o `self`. Isso é
         uma particularidade de Python, vamos falar mais
         disso adiante!"""
@@ -10,10 +11,14 @@ class User:
         self.email = email
         self.password = password
 
+    # metodo de envio de email
+    def reset_password(self):
+        print("Envia email de reset de senha")
+
 
 # instanciando a classe User
 
-meu_user = User('Cleberton', 'clebert@gmail.com', '123')
+meu_user = User("Cleberton", "clebert@gmail.com", "123")
 
 # acessando atributos
 
@@ -21,4 +26,9 @@ print(meu_user)  # <__main__.User object at 0x7fd15e68adc0>
 print(meu_user.name)  # Cleberton
 print(meu_user.email)  # clebert@gmail.com
 print(meu_user.password)  # 123
+meu_user.reset_password()  # Envia email de reset de senha
+
+# Pilares
+# Abstarção
+# Encapsulamento
 
